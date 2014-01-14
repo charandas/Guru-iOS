8b1859202bad67c0d51ed73daf473f6589eb37a8
Karan Batra-Daitch <karanganesha04@gmail.com>
2014-01-13 21:20:23 -0500

    Make UIPhotoEditViewController from dzenbot work for guru

--- guru/GuruViewController.m
+++ guru/GuruViewController.m
@@ -14,19 +14,52 @@
 @interface GuruViewController () <UIScrollViewAccessibilityDelegate>
 
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
+- (void)moveToImageWithTitle:(NSString *)title;
+- (UIImage *)savedImageWithTitle:(NSString *)title;
+- (void)setSavedImage:(UIImage *)image withTitle:(NSString *)title;
 
 @end
 
 @implementation GuruViewController
 
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    static NSString *kUIPhotoPickerDidFinishPickingNotification = @"kUIPhotoPickerDidFinishPickingNotification";
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didPickPhoto:) name:kUIPhotoPickerDidFinishPickingNotification object:nil];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    if (self.title) [self moveToImageWithTitle:self.title];
 }
 
 - (IBAction)launchEdit:(id)sender {
     UIPhotoEditViewController *photoEditViewController = [[UIPhotoEditViewController alloc] initWithImage:self.image cropMode:UIPhotoEditViewControllerCropModeCircular];
     [self.navigationController pushViewController:photoEditViewController animated:YES];
+}
+
+- (void)moveToImageWithTitle:(NSString *)title
+{
+    self.image = [self savedImageWithTitle:title];
+    self.title = title;
+}
+
+- (UIImage *)savedImageWithTitle:(NSString *)title {
+    NSString *key = [NSString stringWithFormat:@"savedImageData.%@", title];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSData* myEncodedImageData = [defaults objectForKey:key];
+    UIImage* image = [UIImage imageWithData:myEncodedImageData];
+    return image;
+}
+
+- (void)setSavedImage:(UIImage *)image withTitle:(NSString *)title {
+    NSString *key = [NSString stringWithFormat:@"savedImageData.%@", title];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSData* imageData = UIImagePNGRepresentation(image);
+    [defaults setObject:imageData forKey:key];
+    [defaults synchronize];
 }
 
 - (UIImage*)image
@@ -58,11 +91,30 @@
     
 }
 
-#pragma mark - UIScrollViewDelegate
-/*- (UIView*)viewForZoomingInScrollView:(UIScrollView *)scrollView
+#pragma mark - UIPhotoPickerController methods
+
+/*
+ * Called by a notification whenever the user picks a photo.
+ */
+- (void)didPickPhoto:(NSNotification *)notification
 {
-    return self.imageView;
-}*/
+    UIImage *image = [notification.userInfo objectForKey:UIImagePickerControllerEditedImage];
+    if (!image) image = [notification.userInfo objectForKey:UIImagePickerControllerOriginalImage];
+    
+    self.image = image;
+    [self setSavedImage:image withTitle:self.title];
+    
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+/*
+ * Called whenever the user cancels the picker.
+ */
+- (void)cancelPicker:(id)sender
+{
+
+}
+
 
 
 @end
